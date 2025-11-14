268f093f78133a845187059b63ca46bb23d51f62
JJAnawat <anawatbumroongsri084@gmail.com>
2025-11-14 16:41:25 +0700

    feat: Handle realtime user, public room changes

--- src/types/socket.types.ts
+++ src/types/socket.types.ts
@@ -1,8 +1,19 @@
+import { PublicRoomDTO } from '@/types/room.types';
+
 // Socket.IO event types
 export interface ServerToClientEvents {
+  // Users events
+  username_set: (data: { username: string }) => void;
+  active_users: (data: { users: string[] }) => void;
+
+  // Public rooms events
+  public_rooms_update: (data: {
+    rooms: PublicRoomDTO[];
+  }) => void;
+
   // Room events
-  room_joined: (data: { 
-    roomToken: string; 
+  room_joined: (data: {
+    roomToken: string;
     participantCount: number;
     participants: string[]; // Array of all nicknames in the room
     messages: any[]; // Message history when joining
@@ -10,22 +21,35 @@ export interface ServerToClientEvents {
   room_left: (data: { nickname: string }) => void;
   room_closed: (data: { reason: string }) => void;
   room_ttl_warning: (data: { expiresIn: number }) => void;
-  
+
   // Message events
   new_message: (data: any) => void;
   message_deleted: (data: { messageId: string }) => void;
-  
+
   // System events
-  user_joined: (data: { nickname: string; participantCount: number; participants: string[] }) => void;
-  user_left: (data: { nickname: string; participantCount: number; participants: string[] }) => void;
+  user_joined: (data: {
+    nickname: string;
+    participantCount: number;
+    participants: string[];
+  }) => void;
+  user_left: (data: {
+    nickname: string;
+    participantCount: number;
+    participants: string[];
+  }) => void;
   error: (data: { message: string; code?: string }) => void;
-  
+
   // Session events
   session_created: (data: { sessionToken: string; nickname: string }) => void;
 }
 
 export interface ClientToServerEvents {
+  // Users events
+  set_username: (data: { username: string }) => void;
+  get_active_users: () => void;
+
   // Room events
+  get_public_rooms: () => void;
   join_room: (data: { roomToken: string; sessionToken: string }) => void;
   leave_room: () => void;
   
